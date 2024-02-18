dcabbc87daab7e3c5ab2bf2ca4ba239f5f19fc71
Anthony Saldana Valle <55422282+ansavanix@users.noreply.github.com>
2024-02-17 22:37:38 -0500

    Add files via upload

--- Drawing.py
+++ Drawing.py
@@ -7,23 +7,31 @@ def randomString(n):
     for i in range(n):
         randChar = chr(random.randint(ord('a'), ord('z')))
         if i != 0:
-            result += " "
+            result += "  "
         result += randChar
     return result
+
+def clear(s):
+    screen.fill("white")
+    for y in range(100, 751, 50):
+        pygame.draw.line(surface=s, color=(0, 0, 0), start_pos=(10, y), end_pos=(590, y), width=3)
+        font = pygame.font.Font(None, 75)
+        text_surface = font.render(randomString(10), True, (120, 120, 120))
+        screen.blit(text_surface, (600 // 2 - text_surface.get_width() // 2, y - 50))
 
 
 # Initialize pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 800))
 clock = pygame.time.Clock()
-screen.fill("white")
+clear(screen)
 running = True
 prevCircle = None
 s = pygame.display.get_surface()
+text = randomString(10)
 
 while running:
-    for y in range(100, 751, 50):
-        pygame.draw.line(surface=s, color=(0, 0, 0), start_pos=(10, y), end_pos=(590, y), width=3)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -43,7 +51,7 @@ while running:
     # Handle clearing whiteboard
     kP = pygame.key.get_pressed()
     if kP[pygame.K_c]:
-        screen.fill("white")
+        clear(screen)
     pygame.display.flip()
     clock.tick(60)
 
